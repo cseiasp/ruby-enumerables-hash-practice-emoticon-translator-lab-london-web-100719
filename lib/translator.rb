f164a3910dd0fcce0f3ad345b067628f15466a36
Caroline S-A <caroline.seilern@hotmail.com>
2019-10-01 13:03:45 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -27,6 +27,17 @@ end
 answer
 end
 
-def get_english_meaning
-  # code goes here
+def get_english_meaning(path, emoticon_symbol)
+  
+  emoti_lib = load_library(path)
+
+if emoti_lib[:get_emoticon].include? emoticon_symbol
+  answer = emoti_lib[:get_emoticon][emoticon_symbol]
+else
+  answer = "Sorry, that emoticon was not found"
+end
+
+answer
+end
+  
 end
