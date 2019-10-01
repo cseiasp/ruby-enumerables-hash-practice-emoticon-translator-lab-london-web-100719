99c4b8ef9ba16624938e21d06ee93be715b67a12
Caroline S-A <caroline.seilern@hotmail.com>
2019-10-01 13:09:28 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -30,13 +30,10 @@ def get_english_meaning(path, emoticon_symbol)
   
   emoti_lib = load_library(path)
 
-#if emoti_lib[:get_emoticon].include? emoticon_symbol
+if emoti_lib[:get_meaning].include? emoticon_symbol
   answer = emoti_lib[:get_meaning][emoticon_symbol]
-#else
-  #answer = "Sorry, that emoticon was not found"
-#end
-
-#answer
-#end
-  
+else
+  answer = "Sorry, that emoticon was not found"
+end
+answer
 end
