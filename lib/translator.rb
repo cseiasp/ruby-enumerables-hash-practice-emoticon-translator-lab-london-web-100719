2ac36f96fc8e24103a290cd3f6bb311e74093512
Caroline S-A <caroline.seilern@hotmail.com>
2019-10-01 13:00:20 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -18,11 +18,12 @@ def get_japanese_emoticon(path, emoticon_symbol)
   
 emoti_lib = load_library(path)
 
-#if emoti_lib.include? emoticon_symbol
+if emoti_lib.include? emoticon_symbol
+  answer = emoti_lib[:get_emoticon][emoticon_symbol]
+else
+  answer = "Sorry, that emoticon was not found"
+end
 
-answer = emoti_lib[:get_emoticon][emoticon_symbol]
-
-#end
 answer
 end
 
